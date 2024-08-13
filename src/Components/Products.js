be3e359c282f2d6fbee85863af489b95bab6d70b
adityasakhale <117585278+adityasakhale@users.noreply.github.com>
2024-08-13 14:09:13 +0530

    Update Products.js

--- src/Components/Products.js
+++ src/Components/Products.js
@@ -9,225 +9,246 @@ const Product = () => {
      <h2>Products</h2>
      <div className='container-card'>
         <div className='card'>
-            <img className='p-img' src='./images/coco.jpg' alt=''></img>
+            <img className='p-img' src='./images/coco.jpg' alt='img'></img>
             <div className='info'>
           
-                <h3>Cocopet | कोकोपीठ</h3>
+                <h3>Cocopet |<br></br> कोकोपीठ</h3>
                 <br></br>
                 <hr></hr>
                 <br></br>
-                <button className='chat'><PiWhatsappLogoBold></PiWhatsappLogoBold>     Chat With Us</button>
+                <br></br>
+                <a className='wp' href="https://wa.me/8421603375" target="_blank"><PiWhatsappLogoBold></PiWhatsappLogoBold>     Chat With Us</a>
 
             </div>
         </div>
         <div className='card'>
-            <img className='p-img' src='./images/circle try.jpg' alt=''></img>
+            <img className='p-img' src='./images/circle try.jpg' alt='img'></img>
             <div className='info'>
           
-                <h3>Circle 70-cup try |70 कप गोल ट्रे</h3>
+                <h3>Circle 70-cup try |<br></br>70 कप गोल ट्रे</h3>
                 <br></br>
                 <hr></hr>
                 <br></br>
-                <button className='chat'><PiWhatsappLogoBold></PiWhatsappLogoBold>    Chat With Us</button>
+                <br></br>
+                <a className='wp' href="https://wa.me/8421603375" target="_blank"><PiWhatsappLogoBold></PiWhatsappLogoBold>     Chat With Us</a>
 
             </div>
         </div>
         <div className='card'>
-            <img className='p-img' src='./images/sqrtry.jpg' alt=''></img>
+            <img className='p-img' src='./images/sqrtry.jpg' alt='img'></img>
             <div className='info'>
            
-                <h3>square 70-cup try | 70 कप चौकोन ट्रे</h3>
+                <h3>square 70-cup try |<br></br> 70 कप चौकोन ट्रे</h3>
                 <br></br>
                 <hr></hr>
                 <br></br>
-                <button className='chat'><PiWhatsappLogoBold></PiWhatsappLogoBold>    Chat With Us</button>
+                <br></br>
+                <a className='wp' href="https://wa.me/8421603375" target="_blank"><PiWhatsappLogoBold></PiWhatsappLogoBold>     Chat With Us</a>
 
             </div>
         </div>
-        <div className='card'>
-            <img className='p-img' src='./images/blackpaper.jpg' alt=''></img>
-            <div className='info'>
-           
-                <h3>Double Dolphin Black Paper | डबल डॉल्फिन काळा कागद</h3>
-                <br></br>
-                <hr></hr>
-                <br></br>
-                <button className='chat'><PiWhatsappLogoBold></PiWhatsappLogoBold>    Chat With Us</button>
-
-            </div>
-        </div>
-        <div className='card'>
-            <img className='p-img' src='./images/gnet.jpg' alt=''></img>
-            <div className='info'>
-           
-                <h3>Green Shed Net | शेडनेट </h3>
-                <p>50%/75%/90% avaliable</p>
-                <br></br>
-                <hr></hr>
-                <br></br>
-                <button className='chat'><PiWhatsappLogoBold></PiWhatsappLogoBold>    Chat With Us</button>
-
-            </div>
-        </div>
-        <div className='card'>
-            <img className='p-img' src='./images/whiteshednet.jpg' alt=''></img>
-            <div className='info'>
-         
-                <h3>White Inset Net |इन्सेट नेट </h3>
-                <p>50%/75%/90% avaliable</p>
-                <br></br>
-                <hr></hr>
-                <br></br>
-                <button className='chat'><PiWhatsappLogoBold></PiWhatsappLogoBold>    Chat With Us</button>
-
-            </div>
-        </div>
-        <div className='card'>
-            <img className='p-img' src='./images/blade.jpg' alt=''></img>
-            <div className='info'>
-          
-                <h3> Cutting Blade | ऊस कटिंग ब्लेड</h3>
-                <br></br>
-                <hr></hr>
-                <br></br>
-                <button className='chat'><PiWhatsappLogoBold></PiWhatsappLogoBold>    Chat With Us</button>
-
-            </div>
-        </div>
-        <div className='card'>
-            <img className='p-img' src='./images/pipe.jpg' alt=''></img>
-            <div className='info'>
-          
-                <h3> Classic Pipe | क्लासिक पाईप बंडल</h3>
-                <br></br>
-                <hr></hr>
-                <br></br>
-                <button className='chat'><PiWhatsappLogoBold></PiWhatsappLogoBold>    Chat With Us</button>
-
-            </div>
-        </div>
-        <div className='card'>
-            <img className='p-img' src='./images/shavr.jpg' alt=''></img>
-            <div className='info'>
         
-                <h3>Gardening Shower| शॉवर</h3>
-                <br></br>
-                <hr></hr>
-                <br></br>
-                <button className='chat'><PiWhatsappLogoBold></PiWhatsappLogoBold>    Chat With Us</button>
-
-            </div>
-        </div>
         <div className='card'>
-            <img className='p-img' src='./images/chunna.jpg' alt=''></img>
-
-            <div className='info'>
-     
-                <h3>Chunna | चुंना</h3>
-                <br></br>
-                <hr></hr>
-                <br></br>
-                <button className='chat'><PiWhatsappLogoBold></PiWhatsappLogoBold>    Chat With Us</button>
-
-            </div>
-        </div>
-        <div className='card'>
-            <img className='p-img' src='./images/crate.png' alt=''></img>
-            <div className='info'>
-          
-                <h3>crate | क्रेट</h3>
-                <br></br>
-                <hr></hr>
-                <br></br>
-                <button className='chat'><PiWhatsappLogoBold></PiWhatsappLogoBold>    Chat With Us</button>
-
-            </div>
-        </div>
-        <div className='card'>
-            <img className='p-img' src='./images/packcraet.jpg' alt=''></img>
-            <div className='info'>
-        
-                <h3>Pack 
-                crate | पॅकक्रेट</h3>
-                <br></br>
-                <hr></hr>
-                <br></br>
-                <button className='chat'><PiWhatsappLogoBold></PiWhatsappLogoBold>    Chat With Us</button>
-
-            </div>
-        </div>
-        <div className='card'>
-            <img className='p-img' src='./images/chair.jpg' alt=''></img>
-            <div className='info'>
-          
-                <h3>Chair | खुर्ची</h3>
-                <br></br>
-                <hr></hr>
-                <br></br>
-                <button className='chat'><PiWhatsappLogoBold></PiWhatsappLogoBold>    Chat With Us</button>
-
-            </div>
-        </div>
-        <div className='card'>
-            <img className='p-img' src='./images/stool.jpg' alt=''></img>
+            <img className='p-img' src='./images/gnet.jpg' alt='img'></img>
             <div className='info'>
            
-                <h3>Plastic stool</h3>
-                <br></br>
+                <h3>Green Shed Net | <br></br> शेडनेट </h3>
+                <p id='size'>available in 50%, 75%, 90%</p>
+                
                 <hr></hr>
                 <br></br>
-                <button className='chat'><PiWhatsappLogoBold></PiWhatsappLogoBold>    Chat With Us</button>
-
-            </div>
-        </div>
-        <div className='card'>
-            <img className='p-img' src='./images/sstool.jpg' alt=''></img>
-            <div className='info'>
-           
-                <h3>Small Plastic stool | स्टूल</h3>
-                <br></br>
-                <hr></hr>
-                <br></br>
-                <button className='chat'><PiWhatsappLogoBold></PiWhatsappLogoBold>    Chat With Us</button>
-
-            </div>
-        </div>
-        <div className='card'>
-            <img className='p-img' src='./images/pakonabag.jpg' alt=''></img>
-            <div className='info'>
-        
-                <h3>pakona bag | पकोना बॅग</h3>
-                <br></br>
-                <hr></hr>
-                <br></br>
-                <br></br>
-                <button className='chat'><PiWhatsappLogoBold></PiWhatsappLogoBold>    Chat With Us</button>
-
-            </div>
-        </div>
-        <div className='card'>
-            <img className='p-img' src='./images/tadpatri.jpg' alt=''></img>
-            <div className='info'>
-         
-                <h3>Tarpaulin | ताडपत्री</h3>
-                <p>Size(12x15,12x18,15x24,18x24)</p>
-                <hr></hr>
                 <br></br>
                 
-                <button className='chat'><PiWhatsappLogoBold></PiWhatsappLogoBold>    Chat With Us</button>
+                <a className='wp' href="https://wa.me/8421603375" target="_blank"><PiWhatsappLogoBold></PiWhatsappLogoBold>     Chat With Us</a>
+
+            </div>
+        </div>
+        <div className='card'>
+            <img className='p-img' src='./images/whiteshednet.jpg' alt='img'></img>
+            <div className='info'>
+         
+                <h3>White Inset Net <br></br>इन्सेट नेट </h3>
+                <p id='size'>available in 50%, 75%, 90% </p>
+                
+                <hr></hr>
+                <br></br>
+                <br></br>
+                <a className='wp' href="https://wa.me/8421603375" target="_blank"><PiWhatsappLogoBold></PiWhatsappLogoBold>     Chat With Us</a>
+
+            </div>
+        </div>
+
+        <div className='card'>
+            <img className='p-img' src='./images/tadpatri.jpg' alt='img'></img>
+            <div className='info'>
+         
+                <h3>Tarpaulin |<br></br> ताडपत्री</h3>
+                <p id='size'>available in 12x15,12x18,15x24,18x24</p>
+                <hr></hr>
+                <br></br>
+                <br></br>
+                <a className='wp' href="https://wa.me/8421603375" target="_blank"><PiWhatsappLogoBold></PiWhatsappLogoBold>     Chat With Us</a>
                
             </div>
         </div>
+
         <div className='card'>
-            <img className='p-img' src='./images/sutli.jpg' alt=''></img>
+            <img className='p-img' src='./images/blackpaper.jpg' alt='img'></img>
             <div className='info'>
-        
-                <h3>Nylon Sutli | नायलॉन सुतळी</h3>
+           
+                <h3>Double Dolphin Black Paper |<br></br> डबल डॉल्फिन काळा कागद</h3>
                 <br></br>
                 <hr></hr>
                 <br></br>
                 <br></br>
-                <button className='chat'><PiWhatsappLogoBold></PiWhatsappLogoBold>    Chat With Us</button>
+                <a className='wp' href="https://wa.me/8421603375" target="_blank"><PiWhatsappLogoBold></PiWhatsappLogoBold>     Chat With Us</a>
+
+            </div>
+        </div>
+
+        <div className='card'>
+            <img className='p-img' src='./images/blade.jpg' alt='img'></img>
+            <div className='info'>
+          
+                <h3> Sugarcane Cutting Blade |<br></br> ऊस कटिंग ब्लेड</h3>
+                <br></br>
+                <hr></hr>
+                <br></br>
+                <br></br>
+                <a className='wp' href="https://wa.me/8421603375" target="_blank"><PiWhatsappLogoBold></PiWhatsappLogoBold>     Chat With Us</a>
+
+            </div>
+        </div>
+        <div className='card'>
+            <img className='p-img' src='./images/pipe.jpg' alt='img'></img>
+            <div className='info'>
+          
+                <h3> Classic Pipe |<br></br> क्लासिक पाईप बंडल</h3>
+                <br></br>
+                <hr></hr>
+                <br></br>
+                <br></br>
+                <a className='wp' href="https://wa.me/8421603375" target="_blank"><PiWhatsappLogoBold></PiWhatsappLogoBold>     Chat With Us</a>
+
+            </div>
+        </div>
+        <div className='card'>
+            <img className='p-img' src='./images/shavr.jpg' alt='img'></img>
+            <div className='info'>
+        
+                <h3>Gardening Shower|<br></br> शॉवर</h3>
+                <br></br>
+                <hr></hr>
+                <br></br>
+                <br></br>
+                <a className='wp' href="https://wa.me/8421603375" target="_blank"><PiWhatsappLogoBold></PiWhatsappLogoBold>     Chat With Us</a>
+
+            </div>
+        </div>
+        <div className='card'>
+            <img className='p-img' src='./images/chunna.jpg' alt='img'></img>
+
+            <div className='info'>
+     
+                <h3>Chunna |<br></br> चुंना</h3>
+                <br></br>
+                <hr></hr>
+                <br></br>
+                <br></br>
+                <a className='wp' href="https://wa.me/8421603375" target="_blank"><PiWhatsappLogoBold></PiWhatsappLogoBold>     Chat With Us</a>
+
+            </div>
+        </div>
+        <div className='card'>
+            <img className='p-img' src='./images/crate.png' alt='img'></img>
+            <div className='info'>
+          
+                <h3>crate |<br></br> क्रेट</h3>
+                <br></br>
+                <hr></hr>
+                <br></br>
+                <br></br>
+                <a className='wp' href="https://wa.me/8421603375" target="_blank"><PiWhatsappLogoBold></PiWhatsappLogoBold>     Chat With Us</a>
+
+            </div>
+        </div>
+        <div className='card'>
+            <img className='p-img' src='./images/packcraet.jpg' alt='img'></img>
+            <div className='info'>
+        
+                <h3>Pack 
+                crate |<br></br> पॅकक्रेट</h3>
+                <br></br>
+                <hr></hr>
+                <br></br>
+                <br></br>
+                <a className='wp' href="https://wa.me/8421603375" target="_blank"><PiWhatsappLogoBold></PiWhatsappLogoBold>     Chat With Us</a>
+
+            </div>
+        </div>
+        <div className='card'>
+            <img className='p-img' src='./images/chair.jpg' alt='img'></img>
+            <div className='info'>
+          
+                <h3>Chair |<br></br> खुर्ची</h3>
+                <br></br>
+                <hr></hr>
+                <br></br>
+                <br></br>
+                <a className='wp' href="https://wa.me/8421603375" target="_blank"><PiWhatsappLogoBold></PiWhatsappLogoBold>     Chat With Us</a>
+
+            </div>
+        </div>
+        <div className='card'>
+            <img className='p-img' src='./images/stool.jpg' alt='img'></img>
+            <div className='info'>
+           
+                <h3>Plastic stool |<br></br>स्टूल</h3>
+                <br></br>
+                <hr></hr>
+                <br></br>
+                <br></br>
+                <a className='wp' href="https://wa.me/8421603375" target="_blank"><PiWhatsappLogoBold></PiWhatsappLogoBold>     Chat With Us</a>
+
+            </div>
+        </div>
+        <div className='card'>
+            <img className='p-img' src='./images/sstool.jpg' alt='img'></img>
+            <div className='info'>
+           
+                <h3>Small Plastic stool |<br></br> स्टूल</h3>
+                <br></br>
+                <hr></hr>
+                <br></br>
+                <br></br>
+                <a className='wp' href="https://wa.me/8421603375" target="_blank"><PiWhatsappLogoBold></PiWhatsappLogoBold>     Chat With Us</a>
+
+            </div>
+        </div>
+        <div className='card'>
+            <img className='p-img' src='./images/pakonabag.jpg' alt='img'></img>
+            <div className='info'>
+        
+                <h3>pakona bag |<br></br> पकोना बॅग</h3>
+                <br></br>
+                <hr></hr>
+                <br></br>
+                <br></br>
+                <a className='wp' href="https://wa.me/8421603375" target="_blank"><PiWhatsappLogoBold></PiWhatsappLogoBold>     Chat With Us</a>
+
+            </div>
+        </div>
+        
+        <div className='card'>
+            <img className='p-img' src='./images/sutli.jpg' alt='img'></img>
+            <div className='info'>
+        
+                <h3>Nylon Sutli |<br></br> नायलॉन सुतळी</h3>
+                <br></br>
+                <hr></hr>
+                <br></br>
+                <br></br>
+                <a className='wp' href="https://wa.me/8421603375" target="_blank"><PiWhatsappLogoBold></PiWhatsappLogoBold>     Chat With Us</a>
                 
             </div>
         </div>
@@ -237,3 +258,4 @@ const Product = () => {
 }
 
 export default Product
+
